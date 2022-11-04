0aac559e3bc3b84a2d711ddf9e349d137bc851f5
Ellis <ellischung98@gmail.com>
2022-11-04 00:18:10 -0400

    start landing page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import React, { useState } from "react";
+import { TextField, Button } from "@mui/material";
 import "./App.css";
 
 function App() {
-  return <div className="App">Market++</div>;
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <div className="App">
+      <div>Market++</div>
+      <div className="container">
+        <TextField
+          label="Username"
+          variant="filled"
+          onChange={(event) => {
+            setUserName(event.target.value);
+          }}
+        />
+        <TextField
+          label="Password"
+          variant="filled"
+          onChange={(event) => {
+            setPassword(event.target.value);
+          }}
+        />
+        <Button variant="outlined">Sign In</Button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
