1c80bbd01db76949a47945ec2cfa6e8c20671775
Ellis <ellischung98@gmail.com>
2022-12-26 23:21:29 -0500

    style components for signin/signup page

--- src/App.js
+++ src/App.js
@@ -1,61 +1,59 @@
-import React, { Component } from "react";
+import React from "react";
 import { HashRouter as Router, Route, NavLink } from "react-router-dom";
 import SignUpForm from "./components/SignUpForm";
 import SignInForm from "./components/SignInForm";
 
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <Router basename="/">
-        <div className="App">
-          <div className="appAside" />
-          <div className="appForm">
-            <div className="pageSwitcher">
-              <NavLink
-                to="/sign-in"
-                activeClassName="pageSwitcherItem-active"
-                className="pageSwitcherItem"
-              >
-                Sign In
-              </NavLink>
-              <NavLink
-                exact
-                to="/"
-                activeClassName="pageSwitcherItem-active"
-                className="pageSwitcherItem"
-              >
-                Sign Up
-              </NavLink>
-            </div>
-
-            <div className="formTitle">
-              <NavLink
-                to="/sign-in"
-                activeClassName="formTitleLink-active"
-                className="formTitleLink"
-              >
-                Sign In
-              </NavLink>{" "}
-              or{" "}
-              <NavLink
-                exact
-                to="/"
-                activeClassName="formTitleLink-active"
-                className="formTitleLink"
-              >
-                Sign Up
-              </NavLink>
-            </div>
-
-            <Route exact path="/" component={SignUpForm} />
-            <Route path="/sign-in" component={SignInForm} />
+function App() {
+  return (
+    <Router basename="/">
+      <div className="App">
+        <div className="appAside" />
+        <div className="appForm">
+          <div className="pageSwitcher">
+            <NavLink
+              to="/sign-in"
+              activeClassName="pageSwitcherItem-active"
+              className="pageSwitcherItem"
+            >
+              Sign In
+            </NavLink>
+            <NavLink
+              exact
+              to="/"
+              activeClassName="pageSwitcherItem-active"
+              className="pageSwitcherItem"
+            >
+              Sign Up
+            </NavLink>
           </div>
+
+          <div className="formTitle">
+            <NavLink
+              to="/sign-in"
+              activeClassName="formTitleLink-active"
+              className="formTitleLink"
+            >
+              Sign In
+            </NavLink>{" "}
+            or{" "}
+            <NavLink
+              exact
+              to="/"
+              activeClassName="formTitleLink-active"
+              className="formTitleLink"
+            >
+              Sign Up
+            </NavLink>
+          </div>
+
+          <Route exact path="/" component={SignUpForm} />
+          <Route path="/sign-in" component={SignInForm} />
         </div>
-      </Router>
-    );
-  }
+      </div>
+    </Router>
+  );
 }
 
 export default App;
