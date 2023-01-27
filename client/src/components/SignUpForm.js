e2bfda3033d18517d3b71492f553597c8dd245c2
Ellis <ellischung98@gmail.com>
2023-01-27 03:41:33 -0500

    added modal for terms of service

--- client/src/components/SignUpForm.js
+++ client/src/components/SignUpForm.js
@@ -2,12 +2,13 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import {
   TextField,
-  FormControlLabel,
   Checkbox,
   Typography,
   InputAdornment,
   IconButton,
   Alert,
+  Modal,
+  Box,
 } from "@mui/material";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
@@ -19,6 +20,7 @@ const SignUpForm = () => {
   const [error, setError] = useState("");
   const [formData, setFormData] = useState(initialState);
   const [showPassword, setShowPassword] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -38,6 +40,10 @@ const SignUpForm = () => {
   const handleClickShowPassword = () => setShowPassword(!showPassword);
 
   const handleMouseDownPassword = () => setShowPassword(!showPassword);
+
+  const handleShowModal = () => setShowModal(true);
+
+  const handleHideModal = () => setShowModal(false);
 
   return (
     <form className="formFields" onSubmit={handleSubmit}>
@@ -85,24 +91,39 @@ const SignUpForm = () => {
         />
       </div>
       <div className="inputFields">
-        <FormControlLabel
-          control={
-            <Checkbox
-              required
-              onChange={(event) => {
-                handleChange(event);
-              }}
-            />
-          }
-          label={
-            <Typography className="checkBoxText">
-              I agree with all statements in the
-              <a href="null" className="checkBoxTermsLink">
-                terms of service
-              </a>
+        <div className="checkBoxFields">
+          <Checkbox
+            required
+            onChange={(event) => {
+              handleChange(event);
+            }}
+          />
+          <Typography className="checkBoxText">
+            I agree with all statements in the{" "}
+            <Typography onClick={handleShowModal} className="checkBoxTermsLink">
+              terms of service.
             </Typography>
-          }
-        />
+          </Typography>
+          <Modal open={showModal} onClose={handleHideModal}>
+            <Box className="modalBox">
+              <Typography variant="h6" component="h2">
+                Terms & Conditions
+              </Typography>
+              <Typography sx={{ mt: 2 }}>
+                If you use this site, you are responsible for maintaining the
+                confidentiality of your account and password and for restricting
+                access to your computer, and you agree to accept responsibility
+                for all activities that occur under your account or password.
+                You may not assign or otherwise transfer your account to any
+                other person or entity. You acknowledge that we are not
+                responsible for third party access to your account that results
+                from theft or misappropriation of your account. We reserve the
+                right to refuse or cancel service, terminate accounts, or remove
+                or edit content in our sole discretion.
+              </Typography>
+            </Box>
+          </Modal>
+        </div>
         {error ? <Alert severity="error">{error}</Alert> : <br />}
       </div>
       <div className="submitField">
