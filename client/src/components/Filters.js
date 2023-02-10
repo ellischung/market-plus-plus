1877b0127432d3d2d0171ed9f29a76d9a9dc211b
Ellis <ellischung98@gmail.com>
2023-02-10 02:06:29 -0500

    filter layout finished

--- client/src/components/Filters.js
+++ client/src/components/Filters.js
@@ -3,6 +3,10 @@ import Radio from "@mui/material/Radio";
 import RadioGroup from "@mui/material/RadioGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import InputAdornment from "@mui/material/InputAdornment";
+import Slider from "@mui/material/Slider";
 import "./Filters.css";
 
 const Filters = () => {
@@ -45,8 +49,31 @@ const Filters = () => {
         />
       </RadioGroup>
       <div className="filterTitle">Price Range</div>
+      <Box className="priceRangeContainer">
+        <TextField
+          label={<div className="filterOption">Min</div>}
+          InputProps={{
+            startAdornment: <InputAdornment position="start">$</InputAdornment>,
+          }}
+        />
+        <div className="priceRangeSeparator">-</div>
+        <TextField
+          label={<div className="filterOption">Max</div>}
+          InputProps={{
+            startAdornment: <InputAdornment position="start">$</InputAdornment>,
+          }}
+        />
+      </Box>
       <div className="filterTitle">Location</div>
+      <TextField label="Ex: New York" />
       <div className="filterTitle">Distance</div>
+      <Slider
+        defaultValue={30}
+        valueLabelDisplay="auto"
+        step={10}
+        min={10}
+        max={110}
+      />
     </div>
   );
 };
