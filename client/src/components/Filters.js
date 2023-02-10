7c3d0e0b4627faf3a96f20c53d4bed4b2c29f780
Ellis <ellischung98@gmail.com>
2023-02-10 00:53:27 -0500

    added radio buttons and checkboxes to filters

--- client/src/components/Filters.js
+++ client/src/components/Filters.js
@@ -1,4 +1,8 @@
 import React from "react";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
 import "./Filters.css";
 
 const Filters = () => {
@@ -6,14 +10,40 @@ const Filters = () => {
     <div className="filterContainer">
       <div className="containerTitle">Filters</div>
       <div className="filterTitle">Platforms</div>
-      <div className="filterOption">Facebook Marketplace</div>
-      <div className="filterOption">eBay</div>
-      <div className="filterOption">OfferUp</div>
-      <div className="filterOption">craigslist</div>
+      <FormControlLabel
+        control={<Checkbox value="facebook" defaultChecked />}
+        label={<div className="filterOption">Facebook Marketplace</div>}
+      />
+      <FormControlLabel
+        control={<Checkbox value="ebay" defaultChecked />}
+        label={<div className="filterOption">eBay</div>}
+      />
+      <FormControlLabel
+        control={<Checkbox value="offerup" defaultChecked />}
+        label={<div className="filterOption">OfferUp</div>}
+      />
+      <FormControlLabel
+        control={<Checkbox value="craigslist" defaultChecked />}
+        label={<div className="filterOption">craigslist</div>}
+      />
       <div className="filterTitle">Sort By</div>
-      <div className="filterOption">Newest first</div>
-      <div className="filterOption">Price - high to low</div>
-      <div className="filterOption">Price - low to high</div>
+      <RadioGroup defaultValue="newest">
+        <FormControlLabel
+          value="newest"
+          control={<Radio />}
+          label={<div className="filterOption">Newest first</div>}
+        />
+        <FormControlLabel
+          value="htol"
+          control={<Radio />}
+          label={<div className="filterOption">Price - high to low</div>}
+        />
+        <FormControlLabel
+          value="ltoh"
+          control={<Radio />}
+          label={<div className="filterOption">Price - low to high</div>}
+        />
+      </RadioGroup>
       <div className="filterTitle">Price Range</div>
       <div className="filterTitle">Location</div>
       <div className="filterTitle">Distance</div>
