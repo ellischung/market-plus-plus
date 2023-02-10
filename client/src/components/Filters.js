59cab1b936371a180a0b8261158645bf22bbc426
Ellis <ellischung98@gmail.com>
2023-02-09 23:21:16 -0500

    added css to filters js

--- client/src/components/Filters.js
+++ client/src/components/Filters.js
@@ -1,22 +1,22 @@
 import React from "react";
+import "./Filters.css";
 
 const Filters = () => {
   return (
-    <div style={{display:"flex", flexDirection:"column"}}>
-      <div>Filters:</div>
-      <br />
-      <div>Platforms:</div>
-      <div>facebook marketplace</div>
-      <div>eBay</div>
-      <div>OfferUp</div>
-      <div>craigslist</div>
-      <br />
-      <div>Sort By:</div>
-      <div>Price - high to low</div>
-      <div>Price - low to high</div>
-      <div>date added</div>
-      <br />
-      <div>Price Range:</div>
+    <div className="filterContainer">
+      <div className="containerTitle">Filters</div>
+      <div className="filterTitle">Platforms</div>
+      <div className="filterOption">Facebook Marketplace</div>
+      <div className="filterOption">eBay</div>
+      <div className="filterOption">OfferUp</div>
+      <div className="filterOption">craigslist</div>
+      <div className="filterTitle">Sort By</div>
+      <div className="filterOption">Newest first</div>
+      <div className="filterOption">Price - high to low</div>
+      <div className="filterOption">Price - low to high</div>
+      <div className="filterTitle">Price Range</div>
+      <div className="filterTitle">Location</div>
+      <div className="filterTitle">Distance</div>
     </div>
   );
 };
