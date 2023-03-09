81498a9aa42713f0a35895196169c7efcb179c76
Ellis <ellischung98@gmail.com>
2023-03-09 18:49:11 -0500

    moved displayResults to home file

--- client/src/components/Feed.js
+++ client/src/components/Feed.js
@@ -2,26 +2,7 @@ import React from "react";
 import { Grid } from "@mui/material";
 import "./Feed.css";
 
-const Feed = ({ craigslistData, filters }) => {
-  const displayResults = (data) => {
-    return data.map((listing) => (
-      <Grid item>
-        <div
-          className="itemContainer"
-          onClick={() => window.open(listing[1].url, "_blank")}
-        >
-          <img
-            style={{ width: "250px" }}
-            alt="temp"
-            src={listing[1].imageUrl}
-          />
-          <text style={{ fontSize: "1.4em" }}>{listing[1].title}</text>
-          <text style={{ fontSize: "1em" }}>{listing[1].price}</text>
-        </div>
-      </Grid>
-    ));
-  };
-
+const Feed = ({ craigslistData, filters, displayResults }) => {
   return (
     <div className="feedContainer">
       <Grid container spacing={5}>
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
-import Navbar from "./Navbar";
 import { useLocation } from "react-router-dom";
+import { Grid, Typography } from "@mui/material";
+import Navbar from "./Navbar";
 import Filters from "./Filters";
 import Feed from "./Feed";
-import "./Home.css";
 import decode from "jwt-decode";
 import axios from "axios";
+import "./Home.css";
 
 const Home = () => {
   const [user, setUser] = useState(JSON.parse(localStorage.getItem("profile")));
@@ -21,11 +22,26 @@ const Home = () => {
 
     API.get(`/craigslistSearch/${input}`)
       .then(({ data }) => {
-        setCraigslistData(Object.entries(data));
+        setCraigslistData(Object.values(data));
       })
       .catch(function (error) {
         console.log(error);
       });
+  };
+
+  const displayResults = (data) => {
+    return data.map((listing) => (
+      <Grid item>
+        <div
+          className="itemContainer"
+          onClick={() => window.open(listing.url, "_blank")}
+        >
+          <img style={{ width: "250px" }} alt="temp" src={listing.imageUrl} />
+          <Typography style={{ fontSize: "1.4em" }}>{listing.title}</Typography>
+          <Typography style={{ fontSize: "1em" }}>{listing.price}</Typography>
+        </div>
+      </Grid>
+    ));
   };
 
   const logout = () => {
@@ -52,7 +68,11 @@ const Home = () => {
       <Navbar setInput={setInput} handleSearch={handleSearch} logout={logout} />
       <div className="container">
         <Filters setFilters={setFilters} />
-        <Feed craigslistData={craigslistData} filters={filters} />
+        <Feed
+          craigslistData={craigslistData}
+          filters={filters}
+          displayResults={displayResults}
+        />
       </div>
     </div>
   );
