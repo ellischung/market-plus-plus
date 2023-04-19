b525f74587562e440dd590ccb0272eef17105c51
Ellis <ellischung98@gmail.com>
2023-04-19 02:32:22 -0400

    added geocoding api call to convert zip to coords

--- server/controllers/search.js
+++ server/controllers/search.js
@@ -106,12 +106,14 @@ export const facebookSearch = async (req, res) => {
   }
   searchQuery += splitSearch[splitSearch.length - 1];
 
+  // helper method to retrieve coordinates from postal code
+  const { lat, lng } = await getCoords(10012);
+
   // temp values
   const distance = 50; // in kilometers, needs to be changed from miles
   const minPrice = 100;
   const maxPrice = 800;
   const sortBy = "CREATION_TIME_DESCEND";
-  // lat and lon needs to generated from postal code
 
   // data from graphql api
   const response = await fetch("https://www.facebook.com/api/graphql/", {
@@ -134,7 +136,7 @@ export const facebookSearch = async (req, res) => {
     },
     referrer: `https://www.facebook.com/marketplace/sanfrancisco/search?minPrice=${minPrice}&maxPrice=${maxPrice}&query=${searchQuery}&exact=false`,
     referrerPolicy: "strict-origin-when-cross-origin",
-    body: `av=0&__user=0&__a=1&__req=3v&__hs=19461.HYP%3Acomet_loggedout_pkg.2.1..0.0&dpr=1&__ccg=EXCELLENT&__rev=1007316664&__s=ea5ou9%3Aps82fh%3A2mqjgu&__hsi=7221993308227985648&__dyn=7xeUmwlEnwn8K2WnFw9-2i5U4e2O1gyUW3qi2K360CEbo19oe8hw2nVE4W0om782Cw8G1nzUO0n24oaEd82lwv89k2C1Fwc61uwPyovwRwlE-U2exi4UaEW2C10wNwwwJK2W5olw8Xxm16waCm7-0iK2S3qazo11E2ZwiU8UdUcobUak1xwmo6O&__csr=jMxaDlq_9RnrqAASQcyozARXuvnCXCCAGm9hopF6CAFoKEwF-iVpbhqKfWBKi5e9xacyUCmieU-hoG6u5USFE8obF8qxK74ifzo5Si7EC4Ehx24oOcwRw72xGU05Vi0cZaUx008f601hNcFQaDwBgbEAw0PO05F46E7tw9iaDwkUvg7e260dQwe-Uco-E3uw2yU0jnw6Mw8mm04TU0OKGG4A8yU8Euhy04qwdaqh02no3gwhd02VU0qXDw0CIBo2Iw9i055C0XU0g68084hFUgw1tW&__comet_req=15&lsd=AVo4efwbayM&jazoest=21029&__spin_r=1007316664&__spin_b=trunk&__spin_t=1681501350&qpl_active_flow_ids=931594241&fb_api_caller_class=RelayModern&fb_api_req_friendly_name=CometMarketplaceSearchContentContainerQuery&variables=%7B%22buyLocation%22%3A%7B%22latitude%22%3A37.7793%2C%22longitude%22%3A-122.419%7D%2C%22contextual_data%22%3Anull%2C%22count%22%3A24%2C%22cursor%22%3Anull%2C%22flashSaleEventID%22%3A%22%22%2C%22hasFlashSaleEventID%22%3Afalse%2C%22marketplaceSearchMetadataCardEnabled%22%3Atrue%2C%22params%22%3A%7B%22bqf%22%3A%7B%22callsite%22%3A%22COMMERCE_MKTPLACE_WWW%22%2C%22query%22%3A%22${searchQuery}%22%7D%2C%22browse_request_params%22%3A%7B%22commerce_enable_local_pickup%22%3Atrue%2C%22commerce_enable_shipping%22%3Atrue%2C%22commerce_search_and_rp_available%22%3Atrue%2C%22commerce_search_and_rp_category_id%22%3A%5B%5D%2C%22commerce_search_and_rp_condition%22%3Anull%2C%22commerce_search_and_rp_ctime_days%22%3Anull%2C%22filter_location_latitude%22%3A37.7793%2C%22filter_location_longitude%22%3A-122.419%2C%22filter_price_lower_bound%22%3A${minPrice}00%2C%22filter_price_upper_bound%22%3A${maxPrice}00%2C%22filter_radius_km%22%3A${distance}%2C%22commerce_search_sort_by%22%3A%22${sortBy}%22%7D%2C%22custom_request_params%22%3A%7B%22browse_context%22%3Anull%2C%22contextual_filters%22%3A%5B%5D%2C%22referral_code%22%3Anull%2C%22saved_search_strid%22%3Anull%2C%22search_vertical%22%3A%22C2C%22%2C%22seo_url%22%3Anull%2C%22surface%22%3A%22SEARCH%22%2C%22virtual_contextual_filters%22%3A%5B%5D%7D%7D%2C%22savedSearchID%22%3Anull%2C%22savedSearchQuery%22%3A%22${searchQuery}%22%2C%22scale%22%3A1%2C%22shouldIncludePopularSearches%22%3Atrue%2C%22topicPageParams%22%3A%7B%22location_id%22%3A%22sanfrancisco%22%2C%22url%22%3Anull%7D%7D&server_timestamps=true&doc_id=6190990404299255`,
+    body: `av=0&__user=0&__a=1&__req=3v&__hs=19461.HYP%3Acomet_loggedout_pkg.2.1..0.0&dpr=1&__ccg=EXCELLENT&__rev=1007316664&__s=ea5ou9%3Aps82fh%3A2mqjgu&__hsi=7221993308227985648&__dyn=7xeUmwlEnwn8K2WnFw9-2i5U4e2O1gyUW3qi2K360CEbo19oe8hw2nVE4W0om782Cw8G1nzUO0n24oaEd82lwv89k2C1Fwc61uwPyovwRwlE-U2exi4UaEW2C10wNwwwJK2W5olw8Xxm16waCm7-0iK2S3qazo11E2ZwiU8UdUcobUak1xwmo6O&__csr=jMxaDlq_9RnrqAASQcyozARXuvnCXCCAGm9hopF6CAFoKEwF-iVpbhqKfWBKi5e9xacyUCmieU-hoG6u5USFE8obF8qxK74ifzo5Si7EC4Ehx24oOcwRw72xGU05Vi0cZaUx008f601hNcFQaDwBgbEAw0PO05F46E7tw9iaDwkUvg7e260dQwe-Uco-E3uw2yU0jnw6Mw8mm04TU0OKGG4A8yU8Euhy04qwdaqh02no3gwhd02VU0qXDw0CIBo2Iw9i055C0XU0g68084hFUgw1tW&__comet_req=15&lsd=AVo4efwbayM&jazoest=21029&__spin_r=1007316664&__spin_b=trunk&__spin_t=1681501350&qpl_active_flow_ids=931594241&fb_api_caller_class=RelayModern&fb_api_req_friendly_name=CometMarketplaceSearchContentContainerQuery&variables=%7B%22buyLocation%22%3A%7B%22latitude%22%3A${lat}%2C%22longitude%22%3A${lng}%7D%2C%22contextual_data%22%3Anull%2C%22count%22%3A24%2C%22cursor%22%3Anull%2C%22flashSaleEventID%22%3A%22%22%2C%22hasFlashSaleEventID%22%3Afalse%2C%22marketplaceSearchMetadataCardEnabled%22%3Atrue%2C%22params%22%3A%7B%22bqf%22%3A%7B%22callsite%22%3A%22COMMERCE_MKTPLACE_WWW%22%2C%22query%22%3A%22${searchQuery}%22%7D%2C%22browse_request_params%22%3A%7B%22commerce_enable_local_pickup%22%3Atrue%2C%22commerce_enable_shipping%22%3Atrue%2C%22commerce_search_and_rp_available%22%3Atrue%2C%22commerce_search_and_rp_category_id%22%3A%5B%5D%2C%22commerce_search_and_rp_condition%22%3Anull%2C%22commerce_search_and_rp_ctime_days%22%3Anull%2C%22filter_location_latitude%22%3A${lat}%2C%22filter_location_longitude%22%3A${lng}%2C%22filter_price_lower_bound%22%3A${minPrice}00%2C%22filter_price_upper_bound%22%3A${maxPrice}00%2C%22filter_radius_km%22%3A${distance}%2C%22commerce_search_sort_by%22%3A%22${sortBy}%22%7D%2C%22custom_request_params%22%3A%7B%22browse_context%22%3Anull%2C%22contextual_filters%22%3A%5B%5D%2C%22referral_code%22%3Anull%2C%22saved_search_strid%22%3Anull%2C%22search_vertical%22%3A%22C2C%22%2C%22seo_url%22%3Anull%2C%22surface%22%3A%22SEARCH%22%2C%22virtual_contextual_filters%22%3A%5B%5D%7D%7D%2C%22savedSearchID%22%3Anull%2C%22savedSearchQuery%22%3A%22${searchQuery}%22%2C%22scale%22%3A1%2C%22shouldIncludePopularSearches%22%3Atrue%2C%22topicPageParams%22%3A%7B%22location_id%22%3A%22sanfrancisco%22%2C%22url%22%3Anull%7D%7D&server_timestamps=true&doc_id=6190990404299255`,
     method: "POST",
     mode: "cors",
     credentials: "include",
@@ -222,4 +224,16 @@ export const offerupSearch = async (req, res) => {
 
   // send results back
   res.json(results);
+};
+
+const getCoords = async (postalCode) => {
+  const apiKey = process.env.API_KEY;
+
+  const geocodingUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${postalCode}&key=${apiKey}`;
+
+  const response = await fetch(geocodingUrl);
+
+  const data = await response.json();
+
+  return data.results[0].geometry.location;
 };
