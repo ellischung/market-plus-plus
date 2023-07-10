57793757a1ef4716bc46d0ed17013cc4be43e525
Edward Chung <edwardchung2024@gmail.com>
2023-07-10 03:26:35 -0400

    updated liked listings

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -55,3 +55,45 @@ export const signup = async (req, res) => {
     res.status(500).json({ message: "Error signup 500." });
   }
 };
+
+// export const updateFavorite = async (req, res) => {
+//   const { userId, listingHash } = req.body;
+//   console.log(userId, listingHash)
+
+//   try {
+//     const updatedUser = await User.findByIdAndUpdate(req.userId, { liked_listing: [...req.listingHash]})
+    
+//     const token = req.headers.authorization.split(" ")[1];
+
+//     res.status(200).json({ result: updatedUser, token });
+//   } catch (error) {
+//     res.status(500).json({ message: 'Error update user 500.' })
+//   }
+// }
+
+export const updateFavorite = async (req, res) => {
+  const { userId, listingHash, isFavorite } = req.body;
+  console.log(userId, listingHash, isFavorite)
+
+  try {
+    // find the user and update their liked_listing, this might be different based on your User schema
+    const user = await User.findById(userId);
+    console.log(user);
+    if (isFavorite) {
+      if (!user.liked_listings.includes(listingHash)) {
+        user.liked_listings.push(listingHash);
+      }
+    } else {
+      user.liked_listings = user.liked_listings.filter(hash => hash !== listingHash);
+    }
+    
+    await user.save();
+    
+    const updatedUser = await User.findById(userId);
+
+    res.status(200).json({ result: updatedUser });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error updating user.' })
+  }
+}
