856a90b749ffbd206ac39de4ae72464e257dafca
Ellis <ellischung98@gmail.com>
2023-08-01 01:39:05 -0400

    added shuffle algorithm for home feed

--- client/src/components/HomeFeed.js
+++ client/src/components/HomeFeed.js
@@ -6,12 +6,37 @@ const HomeFeed = ({
   offerupHomeFeedData,
   displayResults,
 }) => {
+  // combine all home feed data to one to be shuffled later
+  let combinedFeed = [
+    ...craigslistHomeFeedData,
+    ...facebookHomeFeedData,
+    ...offerupHomeFeedData,
+  ];
+
+  // shuffle feed to have a random order
+  const shuffle = (array) => {
+    let currentIndex = array.length;
+    let temporaryValue;
+    let randomIndex;
+
+    // swapping random and current index values
+    while (0 !== currentIndex) {
+      // pick random index
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // swap values
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  };
+
   return (
     <div className="feed-container">
-      {/* shuffle combined array and call displayResults once */}
-      {craigslistHomeFeedData && displayResults(craigslistHomeFeedData)}
-      {facebookHomeFeedData && displayResults(facebookHomeFeedData)}
-      {offerupHomeFeedData && displayResults(offerupHomeFeedData)}
+      {displayResults(shuffle(combinedFeed))}
     </div>
   );
 };
