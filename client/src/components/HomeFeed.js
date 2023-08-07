ec254f4f79318b28c50523213d1e225255c27123
Ellis <ellischung98@gmail.com>
2023-08-07 02:13:15 -0400

    multiple shuffling due to re-rendering issue in HomeFeed fixed

--- client/src/components/HomeFeed.js
+++ client/src/components/HomeFeed.js
@@ -7,38 +7,38 @@ const HomeFeed = ({
   displayResults,
 }) => {
   // combine all home feed data to one to be shuffled later
-  let combinedFeed = [
-    ...craigslistHomeFeedData,
-    ...facebookHomeFeedData,
-    ...offerupHomeFeedData,
-  ];
+  const combinedFeed = React.useMemo(() => {
+    let feeds = [
+      ...craigslistHomeFeedData,
+      ...facebookHomeFeedData,
+      ...offerupHomeFeedData,
+    ];
 
-  // shuffle feed to have a random order
-  const shuffle = (array) => {
-    let currentIndex = array.length;
-    let temporaryValue;
-    let randomIndex;
+    // Knuth shuffle algorithm
+    const shuffleFeed = (array) => {
+      let currentIndex = array.length;
+      let tempValue;
+      let randomIndex;
 
-    // swapping random and current index values
-    while (0 !== currentIndex) {
-      // pick random index
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
+      // swapping random and current index values
+      while (currentIndex !== 0) {
+        // pick random index
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
 
-      // swap values
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
-    }
+        // swap values
+        tempValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = tempValue;
+      }
 
-    return array;
-  };
+      return array;
+    };
 
-  return (
-    <div className="feed-container">
-      {displayResults(shuffle(combinedFeed))}
-    </div>
-  );
+    return shuffleFeed(feeds);
+  }, [craigslistHomeFeedData, facebookHomeFeedData, offerupHomeFeedData]);
+
+  return <div className="feed-container">{displayResults(combinedFeed)}</div>;
 };
 
 export default HomeFeed;
